ff90847ec92412dad073249439bd85b317b3f4be
Luis Dominguez <l.dominguez.aldana@gmail.com>
2021-08-29 21:09:51 -0500

    Ft: Model updated

--- models/MyContactForm.js
+++ models/MyContactForm.js
@@ -1,12 +1,12 @@
 class MyContactForm {
     constructor(id, fname, lname, email, cellphone, company, message) {
       this.id = id;
-      this.fname = fname; // nombre de la mascota (o titulo del anuncio)
-      this.lname = lname; // perro | gato | otro
-      this.email = email; // links a las fotografías
-      this.cellphone = cellphone; // descripción del anuncio
-      this.company = company; // contacto con la persona que anuncia al animalito
-      this.message = message; // muy importante
+      this.fname = fname; 
+      this.lname = lname; 
+      this.email = email; 
+      this.cellphone = cellphone; 
+      this.company = company; 
+      this.message = message; 
     }
   }
   
